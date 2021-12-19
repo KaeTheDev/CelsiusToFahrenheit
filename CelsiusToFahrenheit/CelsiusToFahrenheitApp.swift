cf3165708f9fd645149ac4e430a9efb23e91cb1a
ShakiraReidThomas <kaethedev@gmail.com>
2021-12-19 17:40:51 -0500

    Initial Commit

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelsiusToFahrenheitApp.swift
+//  CelsiusToFahrenheit
+//
+//  Created by Shakira Reid-Thomas on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CelsiusToFahrenheitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
